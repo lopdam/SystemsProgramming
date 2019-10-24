5b84f7f3c25415073871151a0624a549018139df
damian <dennyslopez2013@gmail.com>
2019-10-24 11:21:01 -0500

    validar items

--- Talleres/AlmacenarDatosEstructuradosMemoria/validar.c
+++ Talleres/AlmacenarDatosEstructuradosMemoria/validar.c
@@ -1,6 +1,16 @@
 #include "validar.h"
 #include<ctype.h>
 
+/* Validar
+password debe de tener mínimo 1 letra, mínimo un digito y mínimo 10 caracteres en total
+
+La función validar() debe de retornar:
+0 si no hubo ningún error
+3 si password < 10
+4 si password no contiene letras
+5 si password no contiene dígitos
+
+*/
 int validar(Usuario_t *u)
 {
   u->nombre[0]=toupper(u->nombre[0]);
@@ -8,10 +18,11 @@ int validar(Usuario_t *u)
 
   u->username[0]=tolower(u->nombre[0]);
 
-  for(int i=0;i<sizeof(u->apellido);i++){
-
+  for(int i=0;i<=sizeof(u->apellido);i++){
        u->username[i+1]=tolower(u->apellido[i]);
   }
+
+  
 
     return 0;
 };
