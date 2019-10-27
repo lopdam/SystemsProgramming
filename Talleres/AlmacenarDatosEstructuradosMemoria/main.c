6954e8d6ab5a82e12deeba09b443dc057bf6b3d7
damian <dennyslopez2013@gmail.com>
2019-10-26 23:52:51 -0500

    swicht stamend add

--- Talleres/AlmacenarDatosEstructuradosMemoria/main.c
+++ Talleres/AlmacenarDatosEstructuradosMemoria/main.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include"validar.h"
+#include <stdio.h>
+#include "validar.h"
 
 /*
 Nombre 
@@ -9,19 +9,42 @@ password
 userid  (generado)
 */
 
-int main(){
+int main()
+{
 
     Usuario_t u;
 
     printf("Ingrese Nombre: ");
-    scanf("%s",u.nombre);
+    scanf("%s", u.nombre);
 
     printf("\nIngrese Apellido: ");
-    scanf("%s",u.apellido);
+    scanf("%s", u.apellido);
 
     printf("\nIngrese Contrasenia: ");
-    scanf("%s",u.password);
+    scanf("%s", u.password);
 
-    return validar(&u);
+    printf("\n------------------------------------------------\n");
+
+    int val = validar(&u);
+
+    switch (val)
+    {
+    case 3:
+        printf("Password no size\n");
+        break;
+    case 4:
+        printf("Letra no in password\n");
+        break;
+    case 5:
+        printf("Number no in password\n");
+        break;
+    case 0:
+        printf("\nCorrecto\n");
+        break;
+    default:
+        printf("Ninguna de las Anteriores");
+        break;
+    }
+
+    return val;
 }
-
--- Talleres/AlmacenarDatosEstructuradosMemoria/validar.c
+++ Talleres/AlmacenarDatosEstructuradosMemoria/validar.c
@@ -43,7 +43,6 @@ int validar(Usuario_t *u)
 
   if (strlen(u->password) < 10)
   {
-    printf("Password no size\n");
     return 3;
   }
 
@@ -64,12 +63,10 @@ int validar(Usuario_t *u)
 
   if (letras)
   {
-    printf("Letra no in password\n");
     return 4;
   }
   if (number)
   {
-    printf("Numer no in password\n");
     return 5;
   }
 
