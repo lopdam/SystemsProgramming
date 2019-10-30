bcbc2b68002f23aef12c6652c617df1dc0c41087
dianmoli98 <dianmoli@espol.edu.ec>
2019-10-30 09:38:48 -0500

    Actualizacion dos items

--- Talleres/CompilandoMake/main.c
+++ Talleres/CompilandoMake/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "validar.h"
 #include "guardar.h"
 
@@ -14,38 +15,62 @@ int main()
 {
 
     Usuario_t u;
+    int  opcion,val;
 
-    printf("Ingrese Nombre: ");
-    scanf("%s", u.nombre);
+    do
+    {
+        printf( "\n   1. Desea agregar un usuario:" );
+        printf( "\n   2. Salir." );
+        printf( "\n\n   Introduzca opcicn (1-2): ");
+        scanf( "%d", &opcion );
 
-    printf("\nIngrese Apellido: ");
-    scanf("%s", u.apellido);
+        switch ( opcion )
+        {
+            case 1: 
+            printf("Ingrese Nombre: ");
+            scanf("%s", u.nombre);
 
-    printf("\nIngrese Contrasenia: ");
-    scanf("%s", u.password);
+            printf("\nIngrese Apellido: ");
+            scanf("%s", u.apellido);
 
-    printf("\n------------------------------------------------\n");
+            printf("\nIngrese Contrasenia: ");
+            scanf("%s", u.password);
 
-    int val = validar(&u);
+            printf("\n------------------------------------------------\n");
+            val = validar(&u);
+            
+            break;
 
-    switch (val)
+            case 2:
+            exit(-1);
+            break;}
+
+         /* Fin del anidamiento */
+         switch (val)
     {
     case 3:
-        printf("Password no size\n");
+        fprintf( stderr,"Password no size\n");
         break;
     case 4:
-        printf("Letra no in password\n");
+        fprintf(stderr,"Letra no in password\n");
         break;
     case 5:
-        printf("Number no in password\n");
+        fprintf(stderr,"Number no in password\n");
         break;
     case 0:
-        printf("\nCorrecto\n");
+        fprintf(stderr,"\nCorrecto\n");
         break;
     default:
-        printf("Ninguna de las Anteriores");
+        fprintf(stderr,"Ninguna de las Anteriores");
         break;
     }
 
-    return val;
+    } while ( opcion != 2 );
+
+    
+return val;
+    
+ 
+    
+   
 }
