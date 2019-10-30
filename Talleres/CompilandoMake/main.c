cdbafd014c4c7a6da3d25a5724785b1aa654884b
damian <dennyslopez2013@gmail.com>
2019-10-29 20:02:12 -0500

    add validar.c.h and main anterior

---
+++ Talleres/CompilandoMake/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "validar.h"
+
+/*
+Nombre 
+Apellido
+username (generado)
+password 
+userid  (generado)
+*/
+
+int main()
+{
+
+    Usuario_t u;
+
+    printf("Ingrese Nombre: ");
+    scanf("%s", u.nombre);
+
+    printf("\nIngrese Apellido: ");
+    scanf("%s", u.apellido);
+
+    printf("\nIngrese Contrasenia: ");
+    scanf("%s", u.password);
+
+    printf("\n------------------------------------------------\n");
+
+    int val = validar(&u);
+
+    switch (val)
+    {
+    case 3:
+        printf("Password no size\n");
+        break;
+    case 4:
+        printf("Letra no in password\n");
+        break;
+    case 5:
+        printf("Number no in password\n");
+        break;
+    case 0:
+        printf("\nCorrecto\n");
+        break;
+    default:
+        printf("Ninguna de las Anteriores");
+        break;
+    }
+
+    return val;
+}
---
+++ Talleres/CompilandoMake/validar.c
@@ -0,0 +1,80 @@
+#include "validar.h"
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+#include<time.h>
+#include <stdlib.h>
+
+/* Validar
+password debe de tener mínimo 1 letra, mínimo un digito y mínimo 10 caracteres en total
+
+La función validar() debe de retornar:
+0 si no hubo ningún error
+3 si password < 10
+4 si password no contiene letras
+5 si password no contiene dígitos
+
+*/
+int validar(Usuario_t *u)
+{
+  u->nombre[0] = toupper(u->nombre[0]);
+  u->apellido[0] = toupper(u->apellido[0]);
+  //id
+  srand(time(NULL));
+  u->userid = rand();
+
+  //minuscula Nombre
+  for (int i = 1; i <= strlen(u->nombre); i++)
+  {
+    u->nombre[i] = tolower(u->nombre[i]);
+  }
+  //Minuscula apellido
+  for (int i = 1; i < strlen(u->apellido); i++)
+  {
+    u->apellido[i] = tolower(u->apellido[i]);
+  }
+
+  u->username[0] = tolower(u->nombre[0]);
+  //Agregar apellido al usename
+  for (int i = 0; i <= strlen(u->apellido); i++)
+  {
+    u->username[i + 1] = tolower(u->apellido[i]);
+  }
+
+  if (strlen(u->password) < 10)
+  {
+    return 3;
+  }
+
+  int letras = 1;
+  int number = 1;
+
+  for (int i = 0; i < strlen(u->password); i++)
+  {
+    if (u->password[i] >= 48 && u->password[i] <= 57)
+    {
+      letras = 0;
+    }
+    if ((u->password[i] >= 65 && u->password[i] <= 90) || (u->password[i] >= 97 && u->password[i] <= 122))
+    {
+      number = 0;
+    }
+  }
+
+  if (letras)
+  {
+    return 4;
+  }
+  if (number)
+  {
+    return 5;
+  }
+
+  printf("\nName: %s\n", u->nombre);
+  printf("LastName: %s\n", u->apellido);
+  printf("UserName: %s\n", u->username);
+  printf("PassWord: %s\n", u->password);
+  printf("IdUser: %d\n", u->userid);
+
+  return 0;
+};
